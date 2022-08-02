dd1e94d412ff7df96f76de9287fbfa348d172fb2
Grant Surlyn <grant@sunflowerseastar.com>
2022-08-02 14:42:32 -0400

    add colors; add all cubies

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 import {
   BoxGeometry,
+  Color,
+  Float32BufferAttribute,
   Mesh,
   MeshBasicMaterial,
-  Scene,
   PerspectiveCamera,
-  // TextureLoader,
+  Scene,
   WebGLRenderer,
 } from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 let camera, scene, renderer;
-let cube;
 let controls;
 
-console.log('asdf2');
+const cubieEdgeSize = 100;
+const gapSize = 8;
+
+const posLeft = (cubieEdgeSize + gapSize) * -1;
+const posRight = cubieEdgeSize + gapSize;
+const posCenter = 0;
+
+const leftCenterRight = [posLeft, posCenter, posRight];
 
 init();
 animate();
@@ -26,25 +33,76 @@ function init() {
     1,
     1000
   );
-  camera.position.z = 400;
-
+  camera.position.z = 500;
 
   scene = new Scene();
 
-  // const texture = new TextureLoader().load('textures/crate.gif');
+  const cubieGeometry = new BoxGeometry(100, 100, 100).toNonIndexed();
+  const material = new MeshBasicMaterial({ vertexColors: true });
+  const positionAttribute = cubieGeometry.getAttribute('position');
 
-  const cubeGeometry = new BoxGeometry(100, 100, 100);
-  const material = new MeshBasicMaterial( { color: 0x333333 } );
+  // put all 6 colors on every cubie, 1 per face
+  const baseColors = [
+    0xff8c94, // red
+    0xffd3ad, // orange
+    0x91cdf2, // blue
+    0xb1e597, // green
+    0xeeeeee, // white
+    0xfaedb9, // yellow
+  ];
 
-  mesh = new Mesh(cubeGeometry, material);
-  scene.add(mesh);
+  const color = new Color();
+  let colors = [];
+  for (let i = 0; i < positionAttribute.count; i += 6) {
+    const baseColorIndex = Math.floor(i / 6);
+    color.setHex(baseColors[baseColorIndex]);
+    colors.push(color.r, color.g, color.b);
+    colors.push(color.r, color.g, color.b);
+    colors.push(color.r, color.g, color.b);
+    colors.push(color.r, color.g, color.b);
+    colors.push(color.r, color.g, color.b);
+    colors.push(color.r, color.g, color.b);
+  }
+  cubieGeometry.setAttribute('color', new Float32BufferAttribute(colors, 3));
+
+  // construct a doubly nested array with all cubie positions
+  let cubies = [];
+  for (let i = 0; i < leftCenterRight.length; i += 1) {
+    let layer = [];
+    for (let j = 0; j < leftCenterRight.length; j += 1) {
+      let row = [];
+      for (let k = 0; k < leftCenterRight.length; k += 1) {
+        row.push({
+          x: leftCenterRight[i],
+          y: leftCenterRight[j],
+          z: leftCenterRight[k],
+        });
+      }
+      layer.push(row);
+    }
+    cubies.push(layer);
+  }
+
+  // loop through all the cubies, position them, and add to scene
+  for (let i = 0; i < cubies.length; i += 1) {
+    for (let j = 0; j < cubies.length; j += 1) {
+      for (let k = 0; k < cubies.length; k += 1) {
+        let cubie = new Mesh(cubieGeometry, material);
+        cubie.position.x = cubies[i][j][k].x;
+        cubie.position.y = cubies[i][j][k].y;
+        cubie.position.z = cubies[i][j][k].z;
+
+        scene.add(cubie);
+      }
+    }
+  }
 
   renderer = new WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  controls = new OrbitControls( camera, renderer.domElement );
+  controls = new OrbitControls(camera, renderer.domElement);
 
   window.addEventListener('resize', onWindowResize);
 }
